80980fb60f65e57ea952565403248dafd6ec42f4
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-10-30 20:29:44 +0200

    Initial commit

---
+++ ConversieDiacritice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleFixer
+{
+    internal class ConversieDiacritice
+    {
+        public static void Conversion(string[] lines, string filename){
+            if (lines.Length == 0) { 
+                Console.WriteLine("No line in source file!");
+            }
+            using (StreamWriter sw = new StreamWriter(File.Open(filename, FileMode.Create), Encoding.UTF8))
+            {
+                foreach (string line in lines)
+                {
+                    string new_line = line;
+                    //replace diacritics in new_line
+                    new_line = new_line.Replace('þ', 'ţ'); //t mic
+                    new_line = new_line.Replace('Þ', 'Ț'); //t mare
+                    new_line = new_line.Replace('º', 'ş'); //s mic
+                    new_line = new_line.Replace('ª', 'Ş'); //s mare
+                    // scriem in fisier linia modificata
+                    sw.WriteLine(new_line);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace SubtitleFixer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing file!");
+                return;
+            }
+            string input="start", filename=args[0];
+            File.Copy(filename, filename + ".bak", true);
+            string[] lines= File.ReadAllLines(filename,
+                System.Text.Encoding.GetEncoding(1252));
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                if (input == "?")
+                {
+                    Console.WriteLine("conv == Conversie Diacritice\nsync == Sincronizare subtitrare\nstop == Oprire Program");
+                }
+                if (input == "conv")
+                {
+                    Console.WriteLine("Conversie Diacritice!");
+                    ConversieDiacritice.Conversion(lines, filename);
+                    Console.WriteLine("Terminare Conversie!");
+                }
+                if (input == "sync")
+                {
+                    Console.WriteLine("Sincronizare!");
+
+                    Console.WriteLine("Sincronizare Terminata!");
+                }
+                //Console.WriteLine(input);
+            }
+        }
+    }
+}
